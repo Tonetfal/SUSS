2b1b1f0a9df06ed17235f93203aedec084e06791
MajorTomAW <tom.wrnr.private@gmail.com>
2025-05-16 18:39:51 +0200

    SUSS curve visualization

---
+++ Source/SUSSEditor/Private/SUSSEditorModule.cpp
@@ -0,0 +1,28 @@
+﻿#include "SussConsideration.h"
+#include "Customization/SussConsiderationCustomization.h"
+#include "Modules/ModuleManager.h"
+
+#define LOCTEXT_NAMESPACE "FSUSSEditorModule"
+
+class FSUSSEditorModule : public IModuleInterface
+{
+public:
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
+IMPLEMENT_MODULE(FSUSSEditorModule, SUSSEditor)
+
+
+void FSUSSEditorModule::StartupModule()
+{
+	FPropertyEditorModule& PropertyEditor = FModuleManager::LoadModuleChecked<FPropertyEditorModule>("PropertyEditor");
+	PropertyEditor.RegisterCustomPropertyTypeLayout(FSussConsideration::StaticStruct()->GetFName(),
+		FOnGetPropertyTypeCustomizationInstance::CreateStatic(&FSussConsiderationCustomization::MakeInstance));
+	PropertyEditor.NotifyCustomizationModuleChanged();
+}
+
+void FSUSSEditorModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
